65ed20b213a390d11b5575dcd41226ec8392f2dd
carloscampos15 <carlos2954@hotmail.com>
2020-09-19 15:31:23 -0500

    add vuetiffy and create login view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import router from './router'
+import Vuetify from 'vuetify'
 import App from './views/App'
+import 'vuetify/dist/vuetify.min.css'
 
-require('./bootstrap');
+Vue.use(Vuetify)
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
     components: {
         App
     },
